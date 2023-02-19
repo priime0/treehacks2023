9bbba859551b5ca648ea18141dd1a33082361b6b
Stephen Williams <stephen.williamsb@gmail.com>
2023-02-18 18:35:07 -0800

    Add nav bar on user profile page

---
+++ frontend/src/profileLayout.js
@@ -0,0 +1,84 @@
+import {
+    Button,
+    Flex,
+    FormControl,
+    FormLabel,
+    Heading,
+    Input,
+    Stack,
+    useColorModeValue,
+    HStack,
+    Avatar,
+    AvatarBadge,
+    IconButton,
+    Center,
+  } from '@chakra-ui/react';
+  import { SmallCloseIcon } from '@chakra-ui/icons';
+  import { useNavigate } from 'react-router-dom';
+  
+  export default function ProfileLayout(): JSX.Element {
+    const navigate = useNavigate();
+    return (
+      <Flex
+        minH={'100vh'}
+        //minW={'100vh'}
+        align={'center'}
+        justify={'center'}
+        bg={useColorModeValue('gray.50', 'gray.800')}>
+        <Stack
+          spacing={4}
+          w={'full'}
+          maxW={'xl'}
+          bg={useColorModeValue('white', 'gray.700')}
+          rounded={'xl'}
+          boxShadow={'lg'}
+          p={6}
+          my={12}>
+          <Heading lineHeight={1.1} fontSize={{ base: '2xl', sm: '3xl' }}>
+            Your Profile
+          </Heading>
+          <FormControl id="userName">
+            <FormLabel>User Icon</FormLabel>
+            <Stack direction={['column', 'row']} spacing={6}>
+              <Center>
+                <Avatar size="xl" src="https://bit.ly/sage-adebayo">
+                </Avatar>
+              </Center>
+              <Center w="full">
+                <Button w="full">Change Icon</Button>
+              </Center>
+            </Stack>
+          </FormControl>
+          <FormControl id="userName">
+            <FormLabel>User name</FormLabel>
+            <Input
+              placeholder="UserName"
+              _placeholder={{ color: 'gray.500' }}
+              type="text"
+            />
+          </FormControl>
+          <FormControl id="email">
+            <FormLabel>Email address</FormLabel>
+            <Input
+              placeholder="your-email@example.com"
+              _placeholder={{ color: 'gray.500' }}
+              type="email"
+            />
+          </FormControl>
+          
+          <Stack spacing={6} direction={['column', 'row']}>
+            <Button
+              bg={'red.400'}
+              color={'white'}
+              w="full"
+              onClick={() => navigate("/")}
+              _hover={{
+                bg: 'red.500',
+              }}>
+              Back
+            </Button>
+          </Stack>
+        </Stack>
+      </Flex>
+    );
+  }
